a53f5676c383555493220b8604d450f85fd9902a
Justin Peters <JMarvel@gmail.com>
2022-06-17 12:06:45 -0700

    add basic styling to html, removes contact info

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,11 @@
-//Business Logic for Order-----------------------------
+//Business Logic for Contact-----------------------------
 
-function Order() {
+function OrderBook() {
   this.Pizza = {};
-  this.Contact = {};
+  this.contacts = {};
   this.orderId = 0;
 }
 
-Order.prototype.newOrder = function(Pizza) {
-  Pizza.id = this.orderId();
-  this.pizzas[pizza.id] = Pizza;
-}
-
-Order.prototype.orderId = function() {
-    this.orderId += 1;
-    return this.orderId;
-}
 
 function Pizza(topping1, topping2, size) {
   this.topping1 = topping1;
@@ -33,15 +24,81 @@ Pizza.prototype.pizzaChoice = function () {
 
 //Business Logic for Contact---------------------------------
 
-function Contact(firstName, lastName, phoneNumber, address) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.phoneNumber = phoneNumber;
-  this.address = address;
-};
+// function Contact(firstName, lastName, phoneNumber, address) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.phoneNumber = phoneNumber;
+//   this.address = address;
+// };
 
+// OrderBook.prototype.addContact = function(contact) {
+//   contact.id = this.assignId();
+//   this.contacts[contact.id] = contact;
+// };
+
+// OrderBook.prototype.assignId = function() {
+//   this.currentId += 1;
+//   return this.currentId;
+// }
 
 //User Interface Logic---------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let userOrder = new OrderBook();
+
+
+
+// function confirmContact() {
+//   const contact = userOrder.findContact(contactId);
+//   $("#contact-confrim").show();
+//   $(".first-name").html(contact.firstName);
+//   $(".last-name").html(contact.lastName);
+//   $(".phone-number").html(contact.phoneNumber);
+//   $(".address").html(contact.Address);
+// }
+
+// $(document).ready(function() {
+//   $("form#UserInfo").submit(function(event){
+//   event.preventDefault();
+//   let inputtedFirstName = $("input#first-name").val();
+//   let inputtedLastName = $("input#last-name").val();
+//   let inputtedPhoneNumber = $("input#phone-number").val();
+//   let inputtedAddress= $("input#address").val();
+
+//   $("input#first-name").val("");
+//   $("input#last-name").val("");
+//   $("input#phone-number").val("");
+//   $("input#address").val("")
+
+//   let newOrder = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedAddress)
+//   OrderBook.addContact(newContact);
+//   })
+// })
 
 
 
